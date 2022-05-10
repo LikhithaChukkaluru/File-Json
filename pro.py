7ae3499eac8943782d315d61738af24c3b2f5549
Likhitha Chukkaluru <103958376+LikhithaChukkaluru@users.noreply.github.com>
2022-05-10 16:25:13 +0530

    Add files via upload

---
+++ pro.py
@@ -0,0 +1,92 @@
+# open("likki.txt","x")
+# "create a file"
+
+# a=["\nlikki","\nmanasa","\nusharani","\nthrisha"]
+# f=open("likki.txt","w")
+# f.write("hello! Jai krishna.\n")
+# f.write("i devotee /of jai sri krishna")
+# f.writelines(a)
+# f=open("likki.txt","a")
+# f.write("\nthere are  my siblings")
+# f=open("likki.txt","r")
+# f.read()
+# print(f)
+# for x in f:
+#     print(x)
+
+# f.close()
+
+# s=["rishabh - meerut\n","imtiyaz - delhi\n","nilima - cochin\n","rati - shimla\n","ayishah - delhi\n",
+#     "raghu - shimla\n","naseer - kanpur\n","karthikeyan - delhi\n","salma - jaipur\n","pankaj - delhi\n",
+#     "brijesh - delhi\n","govind - delhi\n","puneet - shimla\n","siddhi - delhi\n","suman - jaipur\n",
+#     "rajeev - shimla\n","mohinder - delhi\n","rajendra - jaipur\n","priyanka - shimla\n","neela - delhi\n",
+#     "sashi - jaipur\n","sarika - delhi\n","deepti - shimla\n","harshad - delhi\n","trishna - raipur\n",
+#     "pradeep - jaipur\n","sekhar - delhi\n","nand - delhi\n","anoop - delhi\n","balwinder - tokyo\n",]
+# f=open("people-exercise.txt","w")
+# f.writelines(s)
+# print(f)
+# f.close
+
+# f=open("people-exercise.txt","r")
+# count=0
+# for  readline in f:
+#     count+=1
+# print(count)
+                                
+# banks_list = ["Kotak\n", "HDFC\n", "RBL\n", "SBI\n", "Bank of Baroda\n"]
+# f=open("file-question3.txt","w")
+# f.writelines(banks_list)
+# print(f)
+# f.close()
+
+# f=open("delhi.txt","w")
+# for i in f :
+#     if "city"=="delhi" in "people-exercise.txt":
+#         print(i)
+
+
+
+# f=open("delhi.txt","w")
+# f.write("imtiyaz - delhi\n")
+# f.write("ayishah  - delhi\n")
+# f.write("karthikeyan - delhi\n")
+# f.write("brijesh - delhi\n")
+# f.write("pankaj  - delhi\n")
+# f.write("govind - delhi\n")
+# f.write(" siddhi- delhi\n")
+# f.write("brijesh- delhi\n")
+# f.write("mohinder - delhi\n")
+# f.write("neela  - delhi\n")
+# f.write("sarika - delhi\n")
+# f.write("harshad- delhi\n")
+# f.write("sekhar  - delhi\n")
+# f.write(" nand - delhi\n")
+# f.write("anoop - delhi\n")
+# print(f)
+# f.close()
+
+# f=open("shimala.txt","w")
+# f.write("rati - shimla\n")
+# f.write("raghu - shimla\n")
+# f.write("puneet - shimla\n")
+# f.write("rajeev - shimla\n")
+# f.write("priyanka - shimla\n")
+# print(f)
+# f.close()
+
+# f=open("others.txt","w")
+# f.write("rishabh - meerut\n")
+# f.write("nilima - cochin\n")
+# f.write("naseer - kanpur\n")
+# f.write("salma - jaipur\n")
+# f.write("suman - jaipur\n")
+# f.write("rajendra - jaipur\n")
+# f.write("sashi - jaipur\n")
+# f.write("trishna - raipur\n")
+# f.write("pradeep - jaipur\n")
+# f.write("balwinder - tokyo\n")
+# print(f)
+# f=open("others.txt","r")
+# f.read()
+# f.close()
+
